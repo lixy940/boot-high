70eaa1ac7ac8acd2f175054dab561d173c2dd9f5
lixy940 <eric0613@yeah.net>
2018-10-12 14:42:01 +0800

    修改sql

--- boot-high/src/main/resources/db/database_config.sql
+++ boot-high/src/main/resources/db/database_config.sql
@@ -13,10 +13,12 @@ CREATE TABLE `database_config` (
   `db_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `db_name` varchar(64) NOT NULL COMMENT '数据库名字',
   `db_type` varchar(64) NOT NULL COMMENT '数据库类型',
+  `area_type` int(4) DEFAULT NULL COMMENT '区域类型：离线：1  专题：2  本地：3  数据结果：10',
   `db_ip` varchar(64) NOT NULL COMMENT '数据库ip',
   `db_port` varchar(32) NOT NULL COMMENT '数据库端口号',
   `db_server_name` varchar(64) NOT NULL COMMENT '服务名称',
   `db_table_schema` varchar(64) DEFAULT NULL COMMENT 'postgresql,同一个库需要区分不同的模式',
+  `db_relkind` varchar(32) DEFAULT NULL COMMENT 'postgresql库区分视图与表 r:表 v:视图',
   `db_user` varchar(64) DEFAULT NULL COMMENT '数据库用户名',
   `db_password` varchar(64) DEFAULT NULL COMMENT '数据库密码',
   `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1 可用 0 不可用',
@@ -29,9 +31,8 @@ CREATE TABLE `database_config` (
 -- ----------------------------
 -- Records of database_config
 -- ----------------------------
-INSERT INTO `database_config` VALUES ('1', '沙盘数据存储库', 'tidb', '192.168.20.xx', '4000', 'pas2', null, 'root', '123456', '1', '2018-05-24 14:39:09', '2018-05-24 17:50:38', 'lis');
-INSERT INTO `database_config` VALUES ('2', '本地数据', 'mysql', 'localhost', '3306', 'db', null, 'root', '123456', '1', '2018-05-24 17:49:19', '2018-05-25 09:21:35', null);
-INSERT INTO `database_config` VALUES ('3', 'oracle数据源1', 'oracle', '192.168.19.xx', '1521', 'BOL', null, 'bol', 'bol', '1', '2018-05-25 09:03:29', '2018-05-25 09:03:29', null);
-INSERT INTO `database_config` VALUES ('4', 'es数据源', 'es', '192.168.19.xx', '9300', 'my-application', null, null, null, '1', '2018-05-28 09:59:42', '2018-05-28 10:00:10', null);
-INSERT INTO `database_config` VALUES ('5', '测试环境数据源', 'mysql', '192.168.19.xx', '3306', 'pas', null, 'root', '123456', '1', '2018-05-28 16:20:54', '2018-05-29 11:32:53', null);
-INSERT INTO `database_config` VALUES ('6', 'postgres数据源', 'postgresql', '192.168.20.xx', '5432', 'PAS2', 'public', 'postgres', '123456', '1', '2018-05-28 16:44:00', '2018-05-29 12:56:10', null);
+INSERT INTO `database_config` ( `db_name`, `db_type`, `area_type`, `db_ip`, `db_port`, `db_server_name`, `db_table_schema`, `db_relkind`, `db_user`, `db_password`, `status`, `create_time`, `update_time`, `create_person_id`) VALUES ( 'tidb', 'mysql', NULL, '192.168.19.xx', '3306', 'uploadlocal', NULL, NULL, 'root', '123456', '1', '2018-05-28 16:20:54', '2018-09-06 15:50:36', NULL);
+INSERT INTO `database_config` ( `db_name`, `db_type`, `area_type`, `db_ip`, `db_port`, `db_server_name`, `db_table_schema`, `db_relkind`, `db_user`, `db_password`, `status`, `create_time`, `update_time`, `create_person_id`) VALUES ('demo数据源', 'postgresql', NULL, '192.168.19.xx', '5432', 'PAS2', 'demo', 'r', 'postgres', '123456', '1', '2018-05-28 16:44:00', '2018-10-09 18:08:50', NULL);
+INSERT INTO `database_config` ( `db_name`, `db_type`, `area_type`, `db_ip`, `db_port`, `db_server_name`, `db_table_schema`, `db_relkind`, `db_user`, `db_password`, `status`, `create_time`, `update_time`, `create_person_id`) VALUES ( 'demo', 'mysql', '3', '192.168.19.xx', '3306', 'demo', NULL, NULL, 'root', '123456', '1', '2018-05-28 16:20:54', '2018-09-12 18:20:04', NULL);
+INSERT INTO `database_config` ( `db_name`, `db_type`, `area_type`, `db_ip`, `db_port`, `db_server_name`, `db_table_schema`, `db_relkind`, `db_user`, `db_password`, `status`, `create_time`, `update_time`, `create_person_id`) VALUES ( 'demo_view', 'postgresql', NULL, '192.168.19.xx', '5432', 'PAS2', 'demo', 'v', 'postgres', '123456', '1', NULL, '2018-10-09 18:27:58', NULL);
+
